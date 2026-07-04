8ee522b0755693f851781f3cec806cee1e2634de
Vit22-2 <vithourakk@gmail.com>
2026-07-04 21:41:07 +0700

    feat: add title

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,10 +1,10 @@
 export default function AddRecipe() {
-  return (
-    <section>
-      <h1>AddRecipe</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, quisquam.
-      </p>
-    </section>
-  );
+	return (
+		<section className="min-w-3x1 mx-auto space-y-6">
+			<h1 className="text-3x1 my-0 font-bold tracking-tight">Add New Recipe</h1>
+			<p className="muted-foreground my-b-2">
+				Share your delicious recipe with the world
+			</p>
+		</section>
+	);
 }
