904aa85db39cb724363e2d377c2c7ca404e8d5d9
SrunTechsean <techseansrun@gmail.com>
2026-07-04 23:26:37 +0700

    feat(RecipeLibrary): store seedRecipes then display the recipe cards

--- src/pages/RecipeLibrary.jsx
+++ src/pages/RecipeLibrary.jsx
@@ -1,15 +1,21 @@
 import CategoryChips from "../components/CategoryChips";
+import RecipeCard from "../components/RecipeCard";
 import SearchBar from "../components/SearchBar";
+import { seedRecipes } from "../data/seedData";
+import { storage } from "../lib/storage";
 
 export default function RecipeLibrary() {
+  storage.setRecipes(seedRecipes);
+  const recipes = storage.getRecipes();
   return (
     <section className="grid gap-3">
       <SearchBar />
       <CategoryChips />
-      <h1>Recipe Library</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, quisquam.
-      </p>
+      <div className="grid lg:grid-cols-4 gap-6 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
     </section>
   );
 }
