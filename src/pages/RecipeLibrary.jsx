c82068759bda4f70c94dea731c72b57a66cc5127
SrunTechsean <techseansrun@gmail.com>
2026-07-06 01:15:08 +0700

    feat(RecipeLibrary): create a filter function that filter through query

--- src/pages/RecipeLibrary.jsx
+++ src/pages/RecipeLibrary.jsx
@@ -1,18 +1,24 @@
 import CategoryChips from "../components/CategoryChips";
 import RecipeCard from "../components/RecipeCard";
 import SearchBar from "../components/SearchBar";
+import { useFilters } from "../contexts/FilterContext";
 import { seedRecipes } from "../data/seedData";
 import { storage } from "../lib/storage";
 
 export default function RecipeLibrary() {
+  const { searchQuery } = useFilters();
   storage.setRecipes(seedRecipes);
-  const recipes = storage.getRecipes();
+
+  const filteredData = seedRecipes.filter((recipe) =>
+    recipe.title.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <section className="grid gap-3">
       <SearchBar />
       <CategoryChips />
       <div className="grid lg:grid-cols-4 gap-6 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
-        {recipes.map((recipe) => (
+        {filteredData.map((recipe) => (
           <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </div>
