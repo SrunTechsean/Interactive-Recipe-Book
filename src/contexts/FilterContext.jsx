523ec9895390050fc73a745cb5d48ccada57ad82
SrunTechsean <techseansrun@gmail.com>
2026-07-06 00:46:17 +0700

    feat(FilterContext); saved filter query and category into setFilters and load it per refresh

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -1,12 +1,40 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { storage } from "../lib/storage";
 
 export const FilterContext = createContext();
 
 export default function FilterProvider({ children }) {
-  const [query, setQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Store obj of filtered query and category
+  useEffect(() => {
+    storage.setFilters({ searchQuery, selectedCategory });
+  }, [searchQuery, selectedCategory]);
+
+  // Load saved Filter
+  useEffect(() => {
+    const savedFilter = storage.getFilters();
+    if (savedFilter) {
+      setSearchQuery(savedFilter.searchQuery || "");
+      setSelectedCategory(savedFilter.selectedCategory || "All");
+    }
+  }, []);
+
+  const clearFilters = () => {
+    setSelectedCategory("All");
+    setSearchQuery();
+  };
 
   return (
-    <FilterContext.Provider value={{ query, setQuery }}>
+    <FilterContext.Provider
+      value={{
+        searchQuery,
+        setSearchQuery,
+        selectedCategory,
+        setSelectedCategory,
+      }}
+    >
       {children}
     </FilterContext.Provider>
   );
