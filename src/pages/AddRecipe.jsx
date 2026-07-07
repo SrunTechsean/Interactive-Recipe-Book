7420cf3da597d187dcea9bbe266c2170a4d1c94c
Vit22-2 <vithourakk@gmail.com>
2026-07-07 10:52:36 +0700

    feat(handleSave): navigate to /recipes after save

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import RecipeDes from "../components/addRecipe-comp/recipeDes";
 import RecipeTable from "../components/addRecipe-comp/recipeTable";
 import RecipeUpload from "../components/addRecipe-comp/recipeUpload";
@@ -9,16 +10,18 @@ import "./AddRecipe.css";
 
 function AddRecipeForm() {
 	const { saveRecipe } = useRecipeForm();
+	const navigate = useNavigate();
 
 	const handleSave = async () => {
 		const recipe = await saveRecipe();
 		console.log("Saved recipe:", recipe);
+		navigate("/recipes");
 	};
 
 	return (
 		<section className="w-full max-w-3xl mx-auto px-4 space-y-6">
 			<h1 className="text-3xl my-0 font-bold tracking-tight">Add New Recipe</h1>
-			<p className="text-brand-text-muted mb-2">
+			<p className="text-muted- mb-2">
 				Share your delicious recipe with the world
 			</p>
 			<form className="space-y-6" onSubmit={(e) => e.preventDefault()}>
