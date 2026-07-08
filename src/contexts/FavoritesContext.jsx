53815ed7e9736ccd16b8160defbd79a45f8004e3
SrunTechsean <techseansrun@gmail.com>
2026-07-09 00:49:26 +0700

    fix(FavoritesContext): make FavoritesProvider the main default function of that file

--- src/contexts/FavoritesContext.jsx
+++ src/contexts/FavoritesContext.jsx
@@ -1,36 +1,8 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { storage } from "../lib/storage";
- 
+
 const FavoritesContext = createContext();
- 
-export function FavoritesProvider({ children }) {
-  const [favorites, setFavorites] = useState([]);
- 
-  // Hydration
-  useEffect(() => {
-    setFavorites(storage.getFavorites());
-  }, []);
- 
-  // Persistence
-  useEffect(() => {
-    storage.setFavorites(favorites);
-  }, [favorites]);
- 
-  const toggleFavorite = (id) => {
-    setFavorites((prev) =>
-      prev.includes(id) ? prev.filter((favId) => favId !== id) : [...prev, id]
-    );
-  };
- 
-  const isFavorite = (id) => favorites.includes(id);
- 
-  return (
-    <FavoritesContext.Provider value={{ favorites, toggleFavorite, isFavorite }}>
-      {children}
-    </FavoritesContext.Provider>
-  );
-}
- 
+
 export function useFavorites() {
   const context = useContext(FavoritesContext);
   if (!context) {
@@ -38,3 +10,34 @@ export function useFavorites() {
   }
   return context;
 }
+
+export default function FavoritesProvider({ children }) {
+  const [favorites, setFavorites] = useState([]);
+
+  // Hydration
+  useEffect(() => {
+    setFavorites(storage.getFavorites());
+  }, []);
+
+  // Persistence
+  useEffect(() => {
+    storage.setFavorites(favorites);
+  }, [favorites]);
+
+  const toggleFavorite = (id) => {
+    setFavorites((prev) =>
+      prev.includes(id) ? prev.filter((favId) => favId !== id) : [...prev, id],
+    );
+  };
+
+  const isFavorite = (id) => favorites.includes(id);
+
+  return (
+    <FavoritesContext.Provider
+      value={{ favorites, toggleFavorite, isFavorite }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+}
+
