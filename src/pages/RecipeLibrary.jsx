58f67b6a0614dc1298e0e20d0318705fcc02c362
SrunTechsean <techseansrun@gmail.com>
2026-07-09 01:04:19 +0700

    feat(RecipeLibrary): stop setting seedRecipes into storage in RecipeLibrary

--- src/pages/RecipeLibrary.jsx
+++ src/pages/RecipeLibrary.jsx
@@ -3,11 +3,9 @@ import GridForCard from "../components/GridForCard";
 import SearchBar from "../components/SearchBar";
 import { useFilters } from "../contexts/FilterContext";
 import { seedRecipes } from "../data/seedData";
-import { storage } from "../lib/storage";
 
 export default function RecipeLibrary() {
   const { searchQuery } = useFilters();
-  storage.setRecipes(seedRecipes);
 
   const filteredData = seedRecipes.filter((recipe) =>
     recipe.title.toLowerCase().includes(searchQuery.toLowerCase()),
