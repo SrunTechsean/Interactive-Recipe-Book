8ff7718043b624616378553d3ed33a14525d8b8e
SrunTechsean <techseansrun@gmail.com>
2026-07-09 15:18:58 +0700

    feat(App): remove App.css import

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Navbar from "./components/Navbar";
 import AddRecipe from "./pages/AddRecipe";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import RecipeLibrary from "./pages/RecipeLibrary";
 import RecipeDetail from "./pages/RecipeDetail";
-import "./styles/App.css";
+import RecipeLibrary from "./pages/RecipeLibrary";
 
 function App() {
   return (
@@ -18,7 +17,7 @@ function App() {
           <Route element={<Favorites />} path="/favorites" />
           <Route element={<AddRecipe />} path="/add" />
           <Route element={<RecipeLibrary />} path="/recipes" />
-          <Route path="/recipes/:id" element={<RecipeDetail />} />
+          <Route element={<RecipeDetail />} path="/recipes/:id" />
         </Routes>
       </main>
     </div>
