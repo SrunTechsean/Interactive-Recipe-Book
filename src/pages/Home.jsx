6657a9efc72bb1eb5c6d622a5bfa797ad04303c2
SrunTechsean <techseansrun@gmail.com>
2026-07-09 16:10:12 +0700

    feat(Home): display recipes in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Carousel from "../components/Carousel";
+import GridForCard from "../components/GridForCard";
 import { seedRecipes } from "../data/seedData";
+import { storage } from "../lib/storage";
 
 const popularIds = ["1", "3", "6", "7"];
 const popularRecipes = seedRecipes.filter((recipe) =>
@@ -7,9 +9,14 @@ const popularRecipes = seedRecipes.filter((recipe) =>
 );
 
 export default function Home() {
+  const data = storage.getRecipes();
   return (
     <section>
       <Carousel recipes={popularRecipes} />
+      <div className="px-8 py-4">
+        <h2 className="text-lg font-semibold pb-4">Our Recipe</h2>
+        <GridForCard data={data} />
+      </div>
     </section>
   );
 }
